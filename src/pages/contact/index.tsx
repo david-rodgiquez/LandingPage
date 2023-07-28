20e9926e72f77a07855beb92de1da288fbb3a3f6
david-rodgiquez <davidrgz085@gmail.com>
2023-07-28 20:29:55 +0700

    contact form: add spinner, error & success message

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,22 +1,168 @@
 import LayoutPage from "@/components/LayoutPage";
+import IconSpinner from "@/components/icons/IconSpinner";
 import axios from "axios";
 import Head from "next/head";
-import type { FormEvent } from "react";
+import { useState, type FormEvent } from "react";
 
-export default function Page() {
-  const onSubmitForm = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const data = Object.fromEntries(new FormData(e.currentTarget)) as {
-      fullName: string;
-      email: string;
-      phone: string;
-      message?: string;
-    };
+type ContactFormPayload = {
+  fullName: string;
+  email: string;
+  phone: string;
+  message?: string;
+};
+
+type Status = "idle" | "success" | "error" | "loading";
+
+interface UseContactBaseReturnFn {
+  status: Status;
+  submit: (data: ContactFormPayload) => Promise<void>;
+}
+
+interface UseContactLoading extends UseContactBaseReturnFn {
+  status: "loading";
+  error: undefined;
+}
+
+interface UseContactSuccess extends UseContactBaseReturnFn {
+  status: "success";
+  error: undefined;
+}
+
+interface UseContactError extends UseContactBaseReturnFn {
+  status: "error";
+  error: string;
+}
+
+interface UseContactIdle extends UseContactBaseReturnFn {
+  status: "idle";
+  error: undefined;
+}
+
+type UseContactReturnFn =
+  | UseContactIdle
+  | UseContactLoading
+  | UseContactError
+  | UseContactSuccess;
+
+function useContact() {
+  const [status, setStatus] = useState<Status>("idle");
+  const [error, setError] = useState<string | undefined>();
+
+  const submit = async (data: ContactFormPayload) => {
+    setStatus("loading");
+    setError(undefined);
 
     try {
       await axios.post("/api/contact", data);
-    } catch (error) {}
+      setStatus("success");
+    } catch (error) {
+      let errorMessage = "";
+
+      if (axios.isAxiosError(error)) {
+        errorMessage = error.response?.data.message || error.message;
+      } else {
+        errorMessage = (error as Error).message;
+      }
+
+      setError(errorMessage);
+      setStatus("error");
+    }
   };
+
+  return { status, submit, error } as UseContactReturnFn;
+}
+
+function ContactFormSection() {
+  const { status, submit, error } = useContact();
+
+  const onSubmitForm = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = Object.fromEntries(
+      new FormData(e.currentTarget)
+    ) as ContactFormPayload;
+
+    submit(data);
+  };
+
+  return (
+    <div className="w-8/12 bg-white dark:bg-[#1C2127] text-black dark:text-white">
+      <div className="w-full py-16 px-28 flex flex-col gap-8">
+        <h3 className="text-4xl font-semibold">Contact Form</h3>
+        {status !== "success" ? (
+          <form onSubmit={onSubmitForm} className="w-full flex flex-col gap-4">
+            <div className="w-full text-xl flex gap-2 flex-col">
+              <label htmlFor="fullName">
+                Full name <span className="text-[#2D72D2]">*</span>
+              </label>
+              <input
+                required
+                id="fullName"
+                type="text"
+                name="fullName"
+                className="rounded-sm dark:text-white dark:focus:text-white py-2 px-4 border outline-none border-[#D3D8DE dark:border-[#252A31] focus:text-[#191C22] bg-white dark:bg-[#111418] focus:bg-[#E4F0FF] transition-colors focus:border-[#2D72D2] dark:focus:border-[#4C90F0]"
+              />
+            </div>
+            <div className="w-full text-xl flex gap-2 flex-col">
+              <label htmlFor="email">
+                Email <span className="text-[#2D72D2]">*</span>
+              </label>
+              <input
+                required
+                id="email"
+                type="email"
+                name="email"
+                className="rounded-sm dark:text-white dark:focus:text-white py-2 px-4 border outline-none border-[#D3D8DE dark:border-[#252A31] focus:text-[#191C22] bg-white dark:bg-[#111418] focus:bg-[#E4F0FF] transition-colors focus:border-[#2D72D2] dark:focus:border-[#4C90F0]"
+              />
+            </div>
+            <div className="w-full text-xl flex gap-2 flex-col">
+              <label htmlFor="phone">
+                Phone <span className="text-[#2D72D2]">*</span>
+              </label>
+              <input
+                required
+                id="phone"
+                type="phone"
+                name="phone"
+                className="rounded-sm dark:text-white dark:focus:text-white py-2 px-4 border outline-none border-[#D3D8DE dark:border-[#252A31] focus:text-[#191C22] bg-white dark:bg-[#111418] focus:bg-[#E4F0FF] transition-colors focus:border-[#2D72D2] dark:focus:border-[#4C90F0]"
+              />
+            </div>
+            <div className="w-full text-xl flex gap-2 flex-col">
+              <label htmlFor="message">What can we help you with?</label>
+              <textarea
+                rows={5}
+                id="message"
+                name="message"
+                className="rounded-sm dark:text-white dark:focus:text-white py-2 px-4 border outline-none border-[#D3D8DE dark:border-[#252A31] focus:text-[#191C22] bg-white dark:bg-[#111418] focus:bg-[#E4F0FF] transition-colors focus:border-[#2D72D2] dark:focus:border-[#4C90F0]"
+              />
+            </div>
+            {status === "error" && (
+              <p className="text-red-600 text-xl">{error}</p>
+            )}
+            <button
+              disabled={status === "loading"}
+              type="submit"
+              className="dark:bg-[#252A31] disabled:opacity-50 disabled:pointer-events-none dark:border-[#ABB3BF] dark:shadow-[3px_3px_0_0_#ffffff] py-3 mt-6 font-berkeley px-6 border-2 border-[#1B283B] rounded-sm flex justify-between items-center w-max gap-10 text-base bg-[#EDEFF2] hover:shadow-none dark:hover:shadow-none transition-shadow shadow-[3px_3px_0_0_#3B3F44]"
+            >
+              {status === "loading" ? (
+                <IconSpinner className="h-6 w-6 text-black" />
+              ) : (
+                "Submit"
+              )}
+            </button>
+          </form>
+        ) : (
+          <div className="w-full flex flex-col gap-2 text-xl dark:text-white">
+            <p>We received your submission!</p>
+            <p>We typically respond within 12-24 hours</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default function Page() {
   return (
     <>
       <Head>
@@ -33,71 +179,7 @@ export default function Page() {
             </h1>
           </div>
           <div className="max-w-7xl px-4 mx-auto mt-32 font-blender flex gap-6">
-            <div className="w-8/12 bg-white dark:bg-[#1C2127] text-black dark:text-white">
-              <div className="w-full py-16 px-28 flex flex-col gap-8">
-                <h3 className="text-4xl font-semibold">Contact Form</h3>
-                <form
-                  onSubmit={onSubmitForm}
-                  className="w-full flex flex-col gap-4"
-                >
-                  {/*  */}
-                  <div className="w-full text-xl flex gap-2 flex-col">
-                    <label htmlFor="fullName">
-                      Full name <span className="text-[#2D72D2]">*</span>
-                    </label>
-                    <input
-                      required
-                      id="fullName"
-                      type="text"
-                      name="fullName"
-                      className="rounded-sm dark:text-white dark:focus:text-white py-2 px-4 border outline-none border-[#D3D8DE dark:border-[#252A31] focus:text-[#191C22] bg-white dark:bg-[#111418] focus:bg-[#E4F0FF] transition-colors focus:border-[#2D72D2] dark:focus:border-[#4C90F0]"
-                    />
-                  </div>
-
-                  <div className="w-full text-xl flex gap-2 flex-col">
-                    <label htmlFor="email">
-                      Email <span className="text-[#2D72D2]">*</span>
-                    </label>
-                    <input
-                      required
-                      id="email"
-                      type="email"
-                      name="email"
-                      className="rounded-sm dark:text-white dark:focus:text-white py-2 px-4 border outline-none border-[#D3D8DE dark:border-[#252A31] focus:text-[#191C22] bg-white dark:bg-[#111418] focus:bg-[#E4F0FF] transition-colors focus:border-[#2D72D2] dark:focus:border-[#4C90F0]"
-                    />
-                  </div>
-
-                  <div className="w-full text-xl flex gap-2 flex-col">
-                    <label htmlFor="phone">
-                      Phone <span className="text-[#2D72D2]">*</span>
-                    </label>
-                    <input
-                      required
-                      id="phone"
-                      type="phone"
-                      name="phone"
-                      className="rounded-sm dark:text-white dark:focus:text-white py-2 px-4 border outline-none border-[#D3D8DE dark:border-[#252A31] focus:text-[#191C22] bg-white dark:bg-[#111418] focus:bg-[#E4F0FF] transition-colors focus:border-[#2D72D2] dark:focus:border-[#4C90F0]"
-                    />
-                  </div>
-                  <div className="w-full text-xl flex gap-2 flex-col">
-                    <label htmlFor="message">What can we help you with?</label>
-                    <textarea
-                      rows={5}
-                      id="message"
-                      name="message"
-                      className="rounded-sm dark:text-white dark:focus:text-white py-2 px-4 border outline-none border-[#D3D8DE dark:border-[#252A31] focus:text-[#191C22] bg-white dark:bg-[#111418] focus:bg-[#E4F0FF] transition-colors focus:border-[#2D72D2] dark:focus:border-[#4C90F0]"
-                    />
-                  </div>
-
-                  <button
-                    type="submit"
-                    className="dark:bg-[#252A31] dark:border-[#ABB3BF] dark:shadow-[3px_3px_0_0_#ffffff] py-3 mt-6 font-berkeley px-6 border-2 border-[#1B283B] rounded-sm flex justify-between items-center w-max gap-10 text-base bg-[#EDEFF2] hover:shadow-none dark:hover:shadow-none transition-shadow shadow-[3px_3px_0_0_#3B3F44]"
-                  >
-                    Submit
-                  </button>
-                </form>
-              </div>
-            </div>
+            <ContactFormSection />
             <div className="w-4/12 bg-white dark:bg-[#1C2127] text-black dark:text-white">
               <div className="w-full py-16 px-28 flex flex-col gap-8">
                 <h3 className="text-4xl font-semibold">Get in touch</h3>
