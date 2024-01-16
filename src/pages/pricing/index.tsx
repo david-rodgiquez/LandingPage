a4fa0cd9ed9f3fdb8d415b6edc2ed2e036da3817
david-rodgiquez <davidrgz085@gmail.com>
2024-01-16 11:13:07 +0700

    reponsive pricing page

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -1,4 +1,5 @@
 import NewLayout from "@/components/NewLayout";
+import IconChevronRight from "@/components/icons/IconChevronRight";
 import Head from "next/head";
 import Link from "next/link";
 import { ComponentProps, useState } from "react";
@@ -90,8 +91,40 @@ function IconXMark(props: ComponentProps<"svg">) {
   );
 }
 
+const planItems = [
+  {
+    title: "Usage",
+    items: ["Members", "File upload", "File upload volumne"],
+  },
+  {
+    title: "Features",
+    items: ["Workspaces", "Integrations", "API and Webhook access", "HOOPS"],
+  },
+  {
+    title: "Security",
+    items: [
+      "Admin roles",
+      "Advanced authentication controls",
+      "Audit log",
+      "Third-party app management",
+      "Domain claiming",
+    ],
+  },
+] as const;
+
+function getPlanItemByTitle(
+  title: Lowercase<(typeof planItems)[number]["title"]>
+) {
+  return planItems.find((plan) => plan.title.toLocaleLowerCase() === title)!
+    .items;
+}
+
 export default function Page() {
   const [periode, setPeriode] = useState<"monthly" | "yearly">("monthly");
+  const [collapsedPlan, setCollapsedPlan] = useState<
+    Array<(typeof plans)[number]["title"]>
+  >([]);
+
   return (
     <>
       <Head>
@@ -99,7 +132,7 @@ export default function Page() {
       </Head>
       <NewLayout>
         <section
-          className="relative flex items-center justify-center min-h-[384px] mb-32
+          className="relative flex items-center justify-center min-h-[384px] mb-20 lg:mb-32
            before:hidden lg:before:block  before:bg-[url('/img/home-bg-line.svg')] before:w-[520px] before:h-[460px] before:absolute before:bg-no-repeat before:left-0 before:bg-right
            after:hidden lg:after:block  after:bg-[url('/img/home-bg-line.svg')] after:w-[520px] after:h-[460px] after:absolute after:bg-no-repeat after:right-0 after:bg-left
             "
@@ -125,7 +158,7 @@ export default function Page() {
                 <button
                   onClick={() => setPeriode(p.value)}
                   key={p.value}
-                  className={`px-6 py-1.5 font-bold text-lg rounded-full border border-white min-w-[160px] ${
+                  className={`px-6 py-1.5 font-bold text-lg rounded-full border border-white min-w-[100px] lg:min-w-[160px] ${
                     periode === p.value ? "text-[#2D72D2] bg-[#E4F0FF]" : ""
                   }`}
                 >
@@ -140,34 +173,10 @@ export default function Page() {
             </div>
           </div>
         </section>
-        <div className="max-w-7xl px-4 w-full mx-auto grid grid-cols-12 -mt-20 gap-12">
-          <div className="col-span-3 gap-8 flex flex-col">
+        <div className="max-w-7xl px-4 w-full mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-12 -mt-20 gap-8">
+          <div className="lg:col-span-3 gap-8 flex-col hidden lg:flex">
             <div className="h-[120px]"></div>
-            {[
-              {
-                title: "Usage",
-                items: ["Members", "File upload", "File upload volumne"],
-              },
-              {
-                title: "Features",
-                items: [
-                  "Workspaces",
-                  "Integrations",
-                  "API and Webhook access",
-                  "HOOPS",
-                ],
-              },
-              {
-                title: "Security",
-                items: [
-                  "Admin roles",
-                  "Advanced authentication controls",
-                  "Audit log",
-                  "Third-party app management",
-                  "Domain claiming",
-                ],
-              },
-            ].map((feature) => (
+            {planItems.map((feature) => (
               <div key={feature.title} className="flex flex-col">
                 <h4 className="text-2xl font-semibold">{feature.title}</h4>
                 <ul className="flex flex-col font-medium text-lg">
@@ -183,85 +192,134 @@ export default function Page() {
               </div>
             ))}
           </div>
-          {plans.map((plan) => (
-            <div
-              key={plan.title}
-              className={`col-span-3 gap-8 flex flex-col  -m-4 p-6 ${
-                plan.shouldHighlight
-                  ? "border rounded-lg border-[#4C90F0] bg-[rgba(245,248,252,0.50)] shadow-[0px_0px_0px_2px_rgba(138,187,255,0.20)]"
-                  : ""
-              }`}
-            >
-              <div className="flex flex-col gap-3 relative">
-                {plan.shouldHighlight && (
-                  <span className="absolute right-0 border border-[#4C90F0] rounded-full bg-[#2D72D2] text-white leading-none px-2 py-1 text-sm font-medium shadow-[0px_0px_0px_2px_#B0D3FF]">
-                    Best value
-                  </span>
-                )}
-                <h3 className="text-2xl font-bold">{plan.title}</h3>
-                {plan.title !== "Enterprise" ? (
-                  <span className="text-4xl font-bold">
-                    {plan.price[periode]}{" "}
-                    <span className="font-medium text-xl text-[#5F6B7C]">
-                      per user /month
+          {plans.map((plan) => {
+            const isCollapsed = collapsedPlan.includes(plan.title);
+
+            return (
+              <div
+                key={plan.title}
+                className={`lg:col-span-3 gap-6 lg:gap-8 flex flex-col lg:-m-4 p-6 rounded-lg ${
+                  plan.shouldHighlight
+                    ? "border border-[#4C90F0] bg-[rgba(245,248,252,0.50)] shadow-[0px_0px_0px_2px_rgba(138,187,255,0.20)]"
+                    : "border lg:border-none"
+                }`}
+              >
+                <div className="flex flex-col gap-3 relative">
+                  {plan.shouldHighlight && (
+                    <span className="absolute right-0 border border-[#4C90F0] rounded-full bg-[#2D72D2] text-white leading-none px-2 py-1 text-sm font-medium shadow-[0px_0px_0px_2px_#B0D3FF]">
+                      Best value
                     </span>
+                  )}
+                  <h3 className="text-2xl font-bold">{plan.title}</h3>
+                  {plan.title !== "Enterprise" ? (
+                    <span className="text-4xl font-bold">
+                      {plan.price[periode]}{" "}
+                      <span className="font-medium text-xl text-[#5F6B7C]">
+                        per user /month
+                      </span>
+                    </span>
+                  ) : (
+                    <span className="text-4xl font-bold">Let&apos;s talk!</span>
+                  )}
+                  <Link
+                    href="#"
+                    className={`text-lg font-bold text-center py-1 transition-colors rounded ${
+                      !plan.shouldHighlight
+                        ? "border border-[#2D72D2] hover:bg-blue-50 text-[#2D72D2]"
+                        : "bg-[#2D72D2] border border-[#2D72D2] hover:border-[#215db0] text-white hover:bg-[#215db0]"
+                    } `}
+                  >
+                    {plan.title !== "Enterprise"
+                      ? "Get Started"
+                      : "Contact sales"}
+                  </Link>
+                </div>
+
+                <div
+                  className={`w-full mt-3 flex flex-col gap-8 lg:gap-[4.275rem] ${
+                    !isCollapsed ? "hidden lg:flex" : ""
+                  }`}
+                >
+                  {(
+                    Object.keys(plan.features) as Array<
+                      keyof (typeof plan)["features"]
+                    >
+                  ).map((featureName) => {
+                    const items =
+                      plan.features[
+                        featureName as keyof (typeof plan)["features"]
+                      ];
+
+                    const itemTitles = getPlanItemByTitle(featureName);
+
+                    return (
+                      <div key={featureName}>
+                        {items.map((feature, i) => (
+                          <div
+                            className="h-11 flex font-medium text-base lg:text-lg items-center border-b-[1px] border-dashed border-b-[#C5CBD3]"
+                            key={`${featureName}-${i}`}
+                          >
+                            <span className="lg:hidden leading-none">
+                              {featureName === "usage" ? (
+                                `${itemTitles[i]} ${feature}`
+                              ) : (
+                                <div className=" flex gap-2 items-center">
+                                  {feature ? <IconCheck /> : <IconXMark />}
+                                  <span>{itemTitles[i]}</span>
+                                </div>
+                              )}
+                            </span>
+                            <span className="hidden lg:block">
+                              {typeof feature === "string" ? (
+                                feature
+                              ) : typeof feature === "boolean" && feature ? (
+                                <IconCheck />
+                              ) : (
+                                <IconXMark />
+                              )}
+                            </span>
+                          </div>
+                        ))}
+                      </div>
+                    );
+                  })}
+                </div>
+                <button
+                  type="button"
+                  onClick={() =>
+                    setCollapsedPlan((prev) => {
+                      if (!prev.includes(plan.title)) {
+                        return prev.concat(plan.title);
+                      }
+                      return prev.filter((p) => p !== plan.title);
+                    })
+                  }
+                  className={`flex gap-2 justify-center items-center lg:hidden`}
+                >
+                  <span className="font-medium">
+                    {!isCollapsed ? "More details" : "Hide details"}
                   </span>
-                ) : (
-                  <span className="text-4xl font-bold">Let’s talk!</span>
-                )}
+                  <IconChevronRight
+                    className={`h-3 w-3 ${
+                      !isCollapsed ? "rotate-90" : "-rotate-90"
+                    }`}
+                  />
+                </button>
                 <Link
                   href="#"
                   className={`text-lg font-bold text-center py-1 transition-colors rounded ${
                     !plan.shouldHighlight
                       ? "border border-[#2D72D2] hover:bg-blue-50 text-[#2D72D2]"
                       : "bg-[#2D72D2] border border-[#2D72D2] hover:border-[#215db0] text-white hover:bg-[#215db0]"
-                  }`}
+                  } ${isCollapsed ? "block" : "hidden lg:block"}`}
                 >
                   {plan.title !== "Enterprise"
                     ? "Get Started"
                     : "Contact sales"}
                 </Link>
               </div>
-
-              <div className="w-full mt-3 flex flex-col gap-[4.275rem]">
-                {Object.keys(plan.features).map((featureName) => {
-                  const items =
-                    plan.features[
-                      featureName as keyof (typeof plan)["features"]
-                    ];
-
-                  return (
-                    <div key={featureName}>
-                      {items.map((feature, i) => (
-                        <div
-                          className="h-11 flex font-medium text-lg items-center border-b-[1px] border-dashed border-b-[#C5CBD3]"
-                          key={`${featureName}-${i}`}
-                        >
-                          {typeof feature === "string" ? (
-                            feature
-                          ) : typeof feature === "boolean" && feature ? (
-                            <IconCheck />
-                          ) : (
-                            <IconXMark />
-                          )}
-                        </div>
-                      ))}
-                    </div>
-                  );
-                })}
-              </div>
-              <Link
-                href="#"
-                className={`text-lg font-bold text-center py-1 transition-colors rounded ${
-                  !plan.shouldHighlight
-                    ? "border border-[#2D72D2] hover:bg-blue-50 text-[#2D72D2]"
-                    : "bg-[#2D72D2] border border-[#2D72D2] hover:border-[#215db0] text-white hover:bg-[#215db0]"
-                }`}
-              >
-                {plan.title !== "Enterprise" ? "Get Started" : "Contact sales"}
-              </Link>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </NewLayout>
     </>
