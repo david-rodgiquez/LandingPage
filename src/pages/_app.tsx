e7ba5c486fad6172dba2f565a6e694d14d18262e
david-rodgiquez <davidrgz085@gmail.com>
2024-01-25 21:02:12 +0700

    add script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -38,6 +38,12 @@ export default function App({ Component, pageProps }: AppProps) {
         }}
         id="segment-analytics"
       />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `!function(t){if(window.ko)return;window.ko=[],["identify","track","removeListeners","open","on","off","qualify","ready"].forEach(function(t){ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}});var n=document.createElement("script");n.async=!0,n.setAttribute("src","https://cdn.getkoala.com/v1/pk_ca3c489ce52ebb366e4a26bf2f66a4176e2d/sdk.js"),(document.body || document.head).appendChild(n)}();`,
+        }}
+        id="koala"
+      />
       <Analytics />
       <ModalCookie />
     </>
